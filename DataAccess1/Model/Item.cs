5cec7d7ca1905a5485853b611f3ab27d754eb043
Josef Janda <josefjanda@email.cz>
2021-11-02 20:40:58 +0100

    adding external code
    Umístit do vedlejsi slozky skladu(jedna se o závislosti do projektu)

---
+++ DataAccess1/Model/Item.cs
@@ -0,0 +1,42 @@
+﻿using DataAccess.Model;
+using DataAccess1.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DataAccess.Model
+{
+    public class Item
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Název je vyžadován")]//validace dat
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Producent je vyžadován")]
+        public string Producer { get; set; }
+        [Required(ErrorMessage = "Rok je vyžadován")]
+        [Range(2000, 2050, ErrorMessage = "Rozsah není 2000-2050")]
+        public int YearProduct { get; set; }
+
+        [Required(ErrorMessage = "Cena je vyžadována")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Producent je vyžadován")]
+        [Range (1,2000,ErrorMessage = "Rozsah není od 1 do 2000")]
+        public int Quantity { get; set; }
+
+       [AllowHtml]
+        public string Description { get; set; }
+
+        public string ImageName { get; set; }
+
+        public ItemCategory Category { get; set; }
+
+
+
+
+    }
+}
---
+++ DataAccess1/NHibernateHelper.cs
@@ -0,0 +1,34 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess1
+{
+    public class NHibernateHelper
+    {
+        //Session objekt zaklad pro session factory-Singlton
+        private static ISessionFactory _factory;
+
+        public static ISession session
+        {
+            get
+            {
+                if (_factory == null)
+                {
+                    var cfg = new Configuration();
+                    _factory = cfg.Configure(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hibernate.cfg.xml")).BuildSessionFactory();
+                }
+                return _factory.OpenSession();
+            }
+
+        }
+
+
+
+    }
+}
